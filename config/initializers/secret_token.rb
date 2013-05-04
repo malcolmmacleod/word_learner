0fa54d81c0d2e46f0eb468a485a1ceb84665b50c
malcolmmacleod <malcolm@macleodhome.com>
2013-05-04 17:01:14 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WordLearner::Application.config.secret_token = 'd1f07a5c40edfdc43ea299eabb7b4a4fdf23c94e4e1a23d70fc628f12d56c02cf137dd8a4d1df81af13c17f74b74f6767f5109ca5aa6de68ad753462cc5de033'
